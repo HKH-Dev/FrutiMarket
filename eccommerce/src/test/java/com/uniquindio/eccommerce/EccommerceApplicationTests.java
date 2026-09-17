f4fe62ef13b8b3353bb90b61d6fa8439bdeac29a
hkh <1kevin@hotmail.es>
2026-09-16 22:57:40 -0500

    Add project

---
+++ eccommerce/src/test/java/com/uniquindio/eccommerce/EccommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.eccommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EccommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
